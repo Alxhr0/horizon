bb6c82f555525fd07c8bc97b1f46204a43d5da7c
Alxhr0 <alxhr0@proton.me>
2025-02-15 23:59:38 +0100

    TEST: MESA from Terra

--- scripts/shared/build-shared.sh
+++ scripts/shared/build-shared.sh
@@ -14,7 +14,7 @@ dnf5 -y copr enable peterwu/rendezvous
 dnf5 -y copr enable sentry/kernel-blu
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
-
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 # Packages
 dnf5 -y install just hourglass bibata-cursor-themes virt-manager fastfetch aria2 virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop nu code
 
@@ -28,10 +28,11 @@ dnf5 -y remove firefox firefox-langpacks
 rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-blu' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
 # Codecs
+rpm-ostree override replace --experimental --from repo="terra-extras" mesa-filesystem mesa-libgbm mesa-libglapi mesa-dri-drivers mesa-libEGL mesa-libGL mesa-vulkan-drivers 
+
 rpm-ostree override remove libavdevice-free ffmpeg-free libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg
 dnf5 -y install intel-media-driver
-rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
-dnf5 -y install mesa-vdpau-drivers-freeworld
+dnf5 -y install mesa-vdpau-drivers
 
 
 systemctl enable podman.socket libvirtd horizon-flatpak-manager
