381ff816c8fca8acfddf1b557655f528881dcc57
Alxhr0 <alxhr0@proton.me>
2025-02-15 03:28:02 +0100

    DEBUG: Try to fix Steam

--- scripts/shared/build-shared.sh
+++ scripts/shared/build-shared.sh
@@ -18,7 +18,7 @@ dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedo
 # Packages
 dnf -y install bibata-cursor-themes virt-manager fastfetch aria2 virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop nu code
 
-dnf -y --setopt=install_weak_deps=False install steam
+dnf5 -y --setopt=install_weak_deps=False install steam
 
 dnf -y remove firefox firefox-langpacks
 
