535945096bced34e9f60c7e47b78e78fb6c35618
Alxhr0 <alxhr0@proton.me>
2025-02-15 02:23:06 +0100

    Rewrite

---
+++ scripts/shared/build-shared.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Repos
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
+
+wget https://download.opensuse.org/repositories/home:paul4us/Fedora_Rawhide/home:paul4us.repo -P /etc/yum.repos.d
+wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
+dnf5 -y copr enable peterwu/rendezvous
+dnf5 -y copr enable sentry/kernel-blu
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+# Packages
+dnf -y install steam bibata-cursor-themes virt-manager fastfetch aria2 virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop nu code
+
+dnf -y remove firefox
+
+# Kernel BLU
+rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-blu' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
+
+systemctl enable podman.socket libvirtd horizon-flatpak-manager
