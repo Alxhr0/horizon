0a76bd9229f2476ee2538133efed07456066ca3e
Alxhr0 <alxhr0@proton.me>
2025-02-16 00:46:10 +0100

    Fix MESA

--- scripts/shared/build-shared.sh
+++ scripts/shared/build-shared.sh
@@ -15,17 +15,6 @@ dnf5 -y copr enable sentry/kernel-blu
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-# Packages
-dnf5 -y install just hourglass bibata-cursor-themes virt-manager fastfetch aria2 virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop nu code terra-release-extras
-
-# Steam
-dnf5 -y --setopt=install_weak_deps=False install steam
-
-# Remove Firefox
-dnf5 -y remove firefox firefox-langpacks
-
-# Kernel BLU
-rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-blu' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
 # Codecs
 sed -i '10s/enabled=1/enabled=0/' /etc/yum.repos.d/terra-mesa.repo 
@@ -48,13 +37,26 @@ packages=(
 )
 
 for pkg in "${packages[@]}"; do
-  dnf5 -y swap $pkg $pkg
+  dnf5 -y swap --enablerepo="terra-mesa" $pkg $pkg
 done
 
 
 rpm-ostree override remove libavdevice-free ffmpeg-free libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpeg
 dnf5 -y install intel-media-driver
 dnf5 -y install mesa-vdpau-drivers
+
+# Packages
+dnf5 -y install just hourglass bibata-cursor-themes virt-manager fastfetch aria2 virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop nu code terra-release-extras
+
+# Steam
+dnf5 -y --setopt=install_weak_deps=False install steam
+
+# Remove Firefox
+dnf5 -y remove firefox firefox-langpacks
+
+# Kernel BLU
+rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-blu' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
 
 
 systemctl enable podman.socket libvirtd horizon-flatpak-manager
