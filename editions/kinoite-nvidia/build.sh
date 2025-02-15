242b9a36abff424f8eb842e0b39faca7abf44f4f
Alxhr0 <alxhr0@proton.me>
2025-02-15 15:32:03 +0100

    Add more Horizon files

--- editions/kinoite-nvidia/build.sh
+++ editions/kinoite-nvidia/build.sh
@@ -5,10 +5,6 @@ set -ouex pipefail
 wget https://download.opensuse.org/repositories/home:paul4us/Fedora_Rawhide/home:paul4us.repo -P /etc/yum.repos.d
 wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 
-
-#dnf5 -y copr enable whitehara/kernel-tkg-zen2 
-#rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:whitehara:kernel-tkg-zen2 kernel
-
 # this installs a package from fedora repos
 dnf install -y kf6-servicemenus-imagetools konsole akmod-nvidia xorg-x11-drv-nvidia-cuda
 
