54ae5b342a43256eb73651b73e452b6584384f14
Alxhr0 <alxhr0@proton.me>
2025-02-16 18:22:14 +0100

    Theming Plasma

---
+++ files/kinoite/usr/share/horizon/scripts/kinoite/theme-plasma.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+plasma-apply-colorscheme AshyDecay
+plasma-apply-cursortheme Bibata-Modern-Ice
+plasma-apply-wallpaperimage /usr/share/horizon/wallpapers/space.png
+
+pkexec rm /etc/xdg/autostart/theme-plasma.desktop
