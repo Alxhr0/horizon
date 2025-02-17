591f5d66e96d418c4e9d94110bef3c59b8c83e5c
Alxhr0 <alxhr0@proton.me>
2025-02-18 00:49:50 +0100

    TEST: Horizon Theme Manager

--- files/kinoite/usr/share/horizon/scripts/kinoite/theme-plasma.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-plasma-apply-colorscheme AshyDecay
-plasma-apply-cursortheme Bibata-Modern-Ice
-plasma-apply-wallpaperimage /usr/share/horizon/wallpapers/space.png
-
-pkexec rm /etc/xdg/autostart/theme-plasma.desktop
